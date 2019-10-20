b220245f46e84ff8529b702756fa8bf5f43d70bc
Anjeyster <anjeyster@gmail.com>
2019-10-20 17:16:57 +0530

    Modify migrations and models

--- database/migrations/2019_05_21_065508_create_songs_table.php
+++ database/migrations/2019_05_21_065508_create_songs_table.php
@@ -21,8 +21,12 @@ class CreateSongsTable extends Migration
             $table->date('released_at')->nullable();
             $table->unsignedBigInteger('added_by');
             $table->unsignedBigInteger('approved_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
             $table->string('details')->nullable();
-            $table->tinyInteger('hash_status')->default(0); //0 - pending, 1 - success, 2 - failed
+            $table->tinyInteger('hash_status')->default(0); //
+//            0 - upload failed, 1 - uploaded, 2 - hash failed, 3 - hash success
+            //0 - pending, 1 - success, 2 - failed
 //            $table->integer('status')->default(1); // 1 - active, 2 - approved (active), 3 - pending, 4 - rejected
             $table->timestamps();
             $table->softDeletes();
